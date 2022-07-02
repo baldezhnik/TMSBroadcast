52b65288fe7fd9a97a792592310701e19823b0b0
Sasha Kuksovsky <specialforicq@gmail.com>
2022-07-02 15:59:47 +0300

    ViewModel

---
+++ app/src/main/java/com/dsd/tmsbroadcast/CatsViewModel.kt
@@ -0,0 +1,83 @@
+package com.dsd.tmsbroadcast
+
+import androidx.lifecycle.*
+import com.google.android.material.progressindicator.CircularProgressIndicator
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class CatsViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
+
+    private val _cat = savedStateHandle.getLiveData<Cat>("cat", createCat("Default"))
+    val cat: MutableLiveData<Cat> = _cat
+
+    private fun createCat(name: String = "Default"): Cat {
+        return Cat(name)
+    }
+
+    fun addPlusOne(param: String) {
+        when (param) {
+            "eat" -> if (_cat.value!!.hunger < 100) {
+                _cat.value!!.hunger += PLUSVALUE
+                _cat.value = _cat.value
+            }
+            "drink" -> if (_cat.value!!.thirst < 100) {
+                _cat.value!!.thirst += PLUSVALUE
+                _cat.value = _cat.value
+            }
+            "care" -> if (_cat.value!!.caress < 100) {
+                _cat.value!!.caress += PLUSVALUE
+                _cat.value = _cat.value
+            }
+        }
+    }
+
+    private suspend fun decreaseParams(param: String) {
+        when (param) {
+            "hunger" -> {
+                if (_cat.value!!.hunger > 0) {
+                    _cat.value!!.hunger -= MINUSVALUE
+                    _cat.value = _cat.value
+                }
+                delay(HUNGERDELAY)
+            }
+            "thirst" -> {
+                if (_cat.value!!.thirst > 0) {
+                    _cat.value!!.thirst -= MINUSVALUE
+                    _cat.value = _cat.value
+                }
+                delay(THIRSTDELAY)
+            }
+            "caress" -> {
+                if (_cat.value!!.caress > 0) {
+                    _cat.value!!.caress -= MINUSVALUE
+                    _cat.value = _cat.value
+                }
+                delay(CARESSDELAY)
+            }
+        }
+    }
+
+    init {
+        if (!savedStateHandle.contains("cat")) {
+            savedStateHandle["cat"] = createCat("Default")
+        }
+
+
+        for (param in listOf("hunger", "thirst", "caress")) {
+            viewModelScope.launch(Dispatchers.Main) {
+                while(true) {
+                    decreaseParams(param)
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val HUNGERDELAY = 6000L
+        const val THIRSTDELAY = 10000L
+        const val CARESSDELAY = 9000L
+        const val PLUSVALUE = 10
+        const val MINUSVALUE = 10
+    }
+}
